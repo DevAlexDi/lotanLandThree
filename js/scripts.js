86355d43c1089179ab8048c717d7242d0053b1b5
keitmark <keitmark@mail.ru>
2019-05-09 13:25:39 +0300

    feat: add footer

--- js/scripts.js
+++ js/scripts.js
@@ -1,31 +1,31 @@
 
 $(document).ready(function(){
-    // $('.slider-baner').slick({
-    //     dots: true,
-    //     arrows: false,
-    //     speed: 500,
-    //     fade: true,
-    //     slidesToShow: 1,
-    //     slidesToScroll: 1
-    // });
+    $('.baner').slick({
+        dots: true,
+        arrows: false,
+        speed: 500,
+        fade: true,
+        slidesToShow: 1,
+        slidesToScroll: 1
+    });
 
-    // $('.news-slider').slick({
-    //     dots: true,
-    //     arrows: false,
-    //     speed: 500,
-    //     slidesToShow: 2,
-    //     slidesToScroll: 2,
-    //     responsive: [
-    //         {
-    //           breakpoint: 768,
-    //           settings: {
-    //             slidesToShow: 1,
-    //             slidesToScroll: 1,
-    //             dots: true
-    //           }
-    //         }
-    //     ]
-    // });
+    $('.news-slider').slick({
+        dots: true,
+        arrows: false,
+        speed: 500,
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        responsive: [
+            {
+              breakpoint: 768,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                dots: true
+              }
+            }
+        ]
+    });
 
     // $('.sm-butt-menu').click(function(){
     //     $('.menu').slideDown(300);
